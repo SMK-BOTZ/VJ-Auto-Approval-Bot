6712ec5e8f31650a7891d4aee7623959c6e602fc
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-09-04 15:25:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,12 +6,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
+    API_ID = int(getenv("API_ID", "28243586"))
+    API_HASH = getenv("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7427875913:AAECLn_AjdTMSizPFEnHQPwm8G855Hi2EIY")
+    FSUB = getenv("FSUB", "Cricketxutkarshfamily")
+    CHID = int(getenv("CHID", "-1002020503341"))
+    SUDO = list(map(int, getenv("SUDO", "1341294921 5340652544 919147646 1844080002").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
